ac529648c8bd6c725401acda2ce66211dae29e75
Gauthier Viau <gauthierviau@gmail.com>
2017-12-05 22:31:45 -0500

    Basic Win32 window

--- shipyard-viewer/main_win32.cpp
+++ shipyard-viewer/main_win32.cpp
@@ -1,6 +1,68 @@
 #include <windows.h>
 
+bool g_IsOpen = false;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CLOSE:
+        DestroyWindow(hWnd);
+        g_IsOpen = false;
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
+
+    return 0;
+}
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+    WNDCLASS wndClass;
+    wndClass.style = CS_HREDRAW | CS_VREDRAW;
+    wndClass.lpfnWndProc = &WndProc;
+    wndClass.cbClsExtra = 0;
+    wndClass.cbWndExtra = 0;
+    wndClass.hInstance = hInstance;
+    wndClass.hIcon = NULL;
+    wndClass.hCursor = 0;
+    wndClass.hbrBackground = 0;
+    wndClass.lpszMenuName = NULL;
+    wndClass.lpszClassName = "Shipyard Viewer";
+
+    if (!RegisterClass(&wndClass))
+    {
+        MessageBox(NULL, "Couldn't register window class", "Win32 error", MB_OK);
+        return 1;
+    }
+
+    HWND windowHandle = CreateWindow("Shipyard Viewer", "Shipyard Viewer", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
+    if (windowHandle == NULL)
+    {
+        MessageBox(NULL, "Couldn't create window", "Win32 error", MB_OK);
+        return 1;
+    }
+
+    ShowWindow(windowHandle, nShowCmd);
+    UpdateWindow(windowHandle);
+
+    g_IsOpen = true;
+
+    MSG msg;
+    while (g_IsOpen)
+    {
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
     return 0;
 }

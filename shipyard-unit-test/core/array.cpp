ed0a47def76c7991b8e9c22a9087df9c87317d59
Gauthier Viau <gauthierviau@gmail.com>
2018-12-28 22:17:43 -0500

    Added unit tests for the Array class, using the Catch single header unit test framework

---
+++ shipyard-unit-test/core/array.cpp
@@ -0,0 +1,167 @@
+#include <catch.hpp>
+
+#include <system/array.h>
+
+#include <algorithm>
+
+TEST_CASE("Test array", "[Array]")
+{
+    Shipyard::Array<int> arr;
+
+    SECTION("push_back")
+    {
+        constexpr int numElements = 3;
+
+        for (int i = 0; i < numElements; i++)
+        {
+            arr.Add(i);
+        }
+
+        REQUIRE(arr.Size() == numElements);
+        REQUIRE(arr.Capacity() >= numElements);
+
+        for (int i = 0; i < numElements; i++)
+        {
+            REQUIRE(arr[i] == i);
+        }
+    }
+
+    SECTION("pop_back")
+    {
+        constexpr int numElements = 3;
+
+        for (int i = 0; i < numElements; i++)
+        {
+            arr.Add(i);
+        }
+
+        REQUIRE(arr.Size() == numElements);
+        REQUIRE(arr.Capacity() >= numElements);
+
+        for (int i = 0; i < numElements; i++)
+        {
+            arr.Pop();
+        }
+
+        REQUIRE(arr.Size() == 0);
+        REQUIRE(arr.Capacity() >= numElements);
+
+        arr.Add(1);
+        arr.Add(2);
+
+        arr.Pop();
+
+        REQUIRE(arr[0] == 1);
+    }
+
+    SECTION("insert")
+    {
+        constexpr int numElements = 3;
+
+        for (int i = 0; i < numElements; i++)
+        {
+            arr.Add(i);
+        }
+
+        arr.InsertAt(0, numElements);
+
+        REQUIRE(arr.Size() == (numElements + 1));
+        REQUIRE(arr.Capacity() >= (numElements + 1));
+        REQUIRE(arr[0] == numElements);
+
+        for (int i = 0; i < numElements; i++)
+        {
+            int idx = i + 1;
+
+            REQUIRE(arr[idx] == i);
+        }
+    }
+
+    SECTION("resize")
+    {
+        arr.Resize(10);
+
+        REQUIRE(arr.Size() == 10);
+        REQUIRE(arr.Capacity() >= 10);
+
+        arr.Resize(5);
+
+        REQUIRE(arr.Size() == 5);
+        REQUIRE(arr.Capacity() >= 10);
+
+        arr.Resize(15);
+
+        REQUIRE(arr.Size() == 15);
+        REQUIRE(arr.Capacity() >= 15);
+    }
+
+    SECTION("reserve")
+    {
+        arr.Reserve(10);
+
+        REQUIRE(arr.Size() == 0);
+        REQUIRE(arr.Capacity() == 10);
+
+        arr.Reserve(5);
+
+        REQUIRE(arr.Capacity() == 10);
+
+        arr.Resize(5);
+
+        REQUIRE(arr.Size() == 5);
+        REQUIRE(arr.Capacity() == 10);
+    }
+
+    SECTION("clear")
+    {
+        constexpr int numElements = 3;
+
+        for (int i = 0; i < numElements; i++)
+        {
+            arr.Add(i);
+        }
+
+        arr.Clear();
+
+        REQUIRE(arr.Size() == 0);
+
+        for (int i = 0; i < numElements; i++)
+        {
+            arr.Add(i);
+        }
+
+        REQUIRE(arr.Size() == numElements);
+    }
+
+    SECTION("empty")
+    {
+        REQUIRE(arr.Empty());
+
+        arr.Add(1);
+
+        REQUIRE(!arr.Empty());
+
+        arr.Pop();
+
+        REQUIRE(arr.Empty());
+    }
+
+    SECTION("forward iterator")
+    {
+        constexpr int numElements = 3;
+        for (int i = 0; i < numElements; i++)
+        {
+            arr.Add(i);
+        }
+
+        int idx = 0;
+
+        Shipyard::Array<int>::Iterator it = arr.begin();
+        for (; it != arr.end(); ++it)
+        {
+            REQUIRE((*it) == idx);
+
+            idx += 1;
+        }
+    }
+}

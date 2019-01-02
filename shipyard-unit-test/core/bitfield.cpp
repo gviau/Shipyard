c5585204c4b5e1d35041d5e89240be287a6f418f
Gauthier Viau <gauthierviau@gmail.com>
2019-01-02 07:34:13 -0500

    Added a class to represent a bitfield of a compile-time known size. Can be arbitrarely large. Uses intrinsics for fast bit scanning. For now, only MSVC compiler is supported, with pragma errors left in places where a proper implementation is needed. Ideally, following implementations should be the same as the one for MSVC, but should provide their platform specific intrinsics for bit scanning

---
+++ shipyard-unit-test/core/bitfield.cpp
@@ -0,0 +1,373 @@
+#include <catch.hpp>
+
+#include <system/bitfield.h>
+
+TEST_CASE("Test bitfield 1 element", "[Bitfield]")
+{
+    Shipyard::Bitfield<NUM_BITS_PER_BITFIELD_ELEMENT> bitfield;
+
+    SECTION("Set bit")
+    {
+        REQUIRE(bitfield.IsClear());
+
+        bitfield.SetBit(0);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(0));
+
+        bitfield.UnsetBit(0);
+
+        REQUIRE(bitfield.IsClear());
+        REQUIRE(!bitfield.IsBitSet(0));
+
+        uint32_t lastBitIndex = (NUM_BITS_PER_BITFIELD_ELEMENT - 1);
+        bitfield.SetBit(lastBitIndex);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(lastBitIndex));
+
+        bitfield.UnsetBit(lastBitIndex);
+
+        REQUIRE(bitfield.IsClear());
+        REQUIRE(!bitfield.IsBitSet(lastBitIndex));
+
+        bitfield.SetBit(10);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(10));
+
+        bitfield.Clear();
+
+        REQUIRE(bitfield.IsClear());
+    }
+
+    SECTION("Set range of bits")
+    {
+        bitfield.SetRange(0, 3);
+
+        for (uint32_t i = 0; i < 3; i++)
+        {
+            REQUIRE(bitfield.IsBitSet(i));
+        }
+
+        bitfield.UnsetRange(0, 3);
+
+        REQUIRE(bitfield.IsClear());
+    }
+
+    SECTION("Get first bit set")
+    {
+        constexpr uint32_t numBits = 3;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            bitfield.SetBit(i);
+        }
+
+        uint32_t firstBitSet = 0;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            REQUIRE(bitfield.GetFirstBitSet(i, firstBitSet));
+            REQUIRE(firstBitSet == i);
+        }
+        
+        REQUIRE(!bitfield.GetFirstBitSet(numBits, firstBitSet));
+
+        bitfield.SetBit(10);
+
+        REQUIRE(bitfield.GetFirstBitSet(numBits, firstBitSet));
+        REQUIRE(firstBitSet == 10);
+    }
+
+    SECTION("Count number of continous bits set")
+    {
+        constexpr uint32_t numBits = 3;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            bitfield.SetBit(i);
+        }
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            uint32_t expectedBitCount = (numBits - i);
+
+            REQUIRE(bitfield.GetLongestRangeWithBitsSet(i) == expectedBitCount);
+        }
+
+        bitfield.Clear();
+
+        bitfield.SetRange(0, NUM_BITS_PER_BITFIELD_ELEMENT - 1);
+
+        REQUIRE(bitfield.GetLongestRangeWithBitsSet(0) == NUM_BITS_PER_BITFIELD_ELEMENT);
+    }
+}
+
+TEST_CASE("Test bitfield 2 elements", "[Bitfield]")
+{
+    constexpr uint32_t bitfieldSize = NUM_BITS_PER_BITFIELD_ELEMENT * 2;
+    Shipyard::Bitfield<bitfieldSize> bitfield;
+
+    SECTION("Set bit")
+    {
+        REQUIRE(bitfield.IsClear());
+
+        bitfield.SetBit(0);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(0));
+
+        bitfield.UnsetBit(0);
+
+        REQUIRE(bitfield.IsClear());
+        REQUIRE(!bitfield.IsBitSet(0));
+
+        uint32_t lastBitIndex = (bitfieldSize - 1);
+        bitfield.SetBit(lastBitIndex);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(lastBitIndex));
+
+        bitfield.UnsetBit(lastBitIndex);
+
+        REQUIRE(bitfield.IsClear());
+        REQUIRE(!bitfield.IsBitSet(lastBitIndex));
+
+        bitfield.SetBit(10);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(10));
+
+        bitfield.Clear();
+
+        REQUIRE(bitfield.IsClear());
+    }
+
+    SECTION("Set range of bits accross elements")
+    {
+        bitfield.SetRange(NUM_BITS_PER_BITFIELD_ELEMENT - 2, NUM_BITS_PER_BITFIELD_ELEMENT + 2);
+
+        for (uint32_t i = NUM_BITS_PER_BITFIELD_ELEMENT - 2; i < NUM_BITS_PER_BITFIELD_ELEMENT + 2; i++)
+        {
+            REQUIRE(bitfield.IsBitSet(i));
+        }
+    }
+
+    SECTION("Get first bit set")
+    {
+        constexpr uint32_t numBits = 3;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            uint32_t bitIndex = i + NUM_BITS_PER_BITFIELD_ELEMENT;
+
+            bitfield.SetBit(bitIndex);
+        }
+
+        uint32_t firstBitSet = 0;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            REQUIRE(bitfield.GetFirstBitSet(i, firstBitSet));
+            REQUIRE(firstBitSet == NUM_BITS_PER_BITFIELD_ELEMENT);
+        }
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            uint32_t bitIndex = i + NUM_BITS_PER_BITFIELD_ELEMENT;
+
+            REQUIRE(bitfield.GetFirstBitSet(bitIndex, firstBitSet));
+            REQUIRE(firstBitSet == bitIndex);
+        }
+
+        REQUIRE(!bitfield.GetFirstBitSet(numBits + NUM_BITS_PER_BITFIELD_ELEMENT, firstBitSet));
+
+        uint32_t bitIndex = 10 + NUM_BITS_PER_BITFIELD_ELEMENT;
+        bitfield.SetBit(bitIndex);
+
+        REQUIRE(bitfield.GetFirstBitSet(numBits + NUM_BITS_PER_BITFIELD_ELEMENT, firstBitSet));
+        REQUIRE(firstBitSet == bitIndex);
+    }
+
+    SECTION("Count number of continous bits set")
+    {
+        constexpr uint32_t numBits = 3;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            uint32_t bitIndex = i + NUM_BITS_PER_BITFIELD_ELEMENT;
+            bitfield.SetBit(bitIndex);
+        }
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            uint32_t expectedBitCount = (numBits - i);
+
+            uint32_t bitIndex = i + NUM_BITS_PER_BITFIELD_ELEMENT;
+            REQUIRE(bitfield.GetLongestRangeWithBitsSet(bitIndex) == expectedBitCount);
+        }
+
+        bitfield.Clear();
+
+        bitfield.SetRange(0, bitfieldSize - 1);
+
+        REQUIRE(bitfield.GetLongestRangeWithBitsSet(0) == bitfieldSize);
+    }
+
+    SECTION("Count number of continous bits set accross bitfield elements")
+    {
+        bitfield.SetBit(NUM_BITS_PER_BITFIELD_ELEMENT - 2);
+        bitfield.SetBit(NUM_BITS_PER_BITFIELD_ELEMENT - 1);
+        bitfield.SetBit(NUM_BITS_PER_BITFIELD_ELEMENT    );
+        bitfield.SetBit(NUM_BITS_PER_BITFIELD_ELEMENT + 1);
+
+        uint32_t startingBitIndex = NUM_BITS_PER_BITFIELD_ELEMENT - 2;
+        for (uint32_t i = 0; i < 4; i++)
+        {
+            uint32_t expectedBitCount = (4 - i);
+
+            uint32_t bitIndex = startingBitIndex + i;
+            REQUIRE(bitfield.GetLongestRangeWithBitsSet(bitIndex) == expectedBitCount);
+        }
+    }
+}
+
+TEST_CASE("Test bitfield 3 elements", "[Bitfield]")
+{
+    constexpr uint32_t bitfieldSize = NUM_BITS_PER_BITFIELD_ELEMENT * 3;
+    Shipyard::Bitfield<bitfieldSize> bitfield;
+
+    SECTION("Set bit")
+    {
+        REQUIRE(bitfield.IsClear());
+
+        bitfield.SetBit(0);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(0));
+
+        bitfield.UnsetBit(0);
+
+        REQUIRE(bitfield.IsClear());
+        REQUIRE(!bitfield.IsBitSet(0));
+
+        uint32_t lastBitIndex = (bitfieldSize - 1);
+        bitfield.SetBit(lastBitIndex);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(lastBitIndex));
+
+        bitfield.UnsetBit(lastBitIndex);
+
+        REQUIRE(bitfield.IsClear());
+        REQUIRE(!bitfield.IsBitSet(lastBitIndex));
+
+        bitfield.SetBit(10);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(10));
+
+        bitfield.Clear();
+
+        REQUIRE(bitfield.IsClear());
+    }
+
+    SECTION("Set range of bits accross elements")
+    {
+        bitfield.SetRange(NUM_BITS_PER_BITFIELD_ELEMENT - 2, NUM_BITS_PER_BITFIELD_ELEMENT + 2);
+
+        for (uint32_t i = NUM_BITS_PER_BITFIELD_ELEMENT - 2; i < NUM_BITS_PER_BITFIELD_ELEMENT + 2; i++)
+        {
+            REQUIRE(bitfield.IsBitSet(i));
+        }
+
+        bitfield.Clear();
+
+        uint32_t endingBitIndex = (NUM_BITS_PER_BITFIELD_ELEMENT * 2 + 2);
+        bitfield.SetRange(0, endingBitIndex - 1);
+
+        for (uint32_t i = 0; i < endingBitIndex; i++)
+        {
+            REQUIRE(bitfield.IsBitSet(i));
+        }
+    }
+
+    SECTION("Get first bit set")
+    {
+        constexpr uint32_t numBits = 3;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            uint32_t bitIndex = i + NUM_BITS_PER_BITFIELD_ELEMENT;
+
+            bitfield.SetBit(bitIndex);
+        }
+
+        uint32_t firstBitSet = 0;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            REQUIRE(bitfield.GetFirstBitSet(i, firstBitSet));
+            REQUIRE(firstBitSet == NUM_BITS_PER_BITFIELD_ELEMENT);
+        }
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            uint32_t bitIndex = i + NUM_BITS_PER_BITFIELD_ELEMENT;
+
+            REQUIRE(bitfield.GetFirstBitSet(bitIndex, firstBitSet));
+            REQUIRE(firstBitSet == bitIndex);
+        }
+
+        REQUIRE(!bitfield.GetFirstBitSet(numBits + NUM_BITS_PER_BITFIELD_ELEMENT, firstBitSet));
+
+        uint32_t bitIndex = 10 + NUM_BITS_PER_BITFIELD_ELEMENT;
+        bitfield.SetBit(bitIndex);
+
+        REQUIRE(bitfield.GetFirstBitSet(numBits + NUM_BITS_PER_BITFIELD_ELEMENT, firstBitSet));
+        REQUIRE(firstBitSet == bitIndex);
+    }
+
+    SECTION("Count number of continous bits set")
+    {
+        constexpr uint32_t numBits = 3;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            uint32_t bitIndex = i + NUM_BITS_PER_BITFIELD_ELEMENT;
+            bitfield.SetBit(bitIndex);
+        }
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            uint32_t expectedBitCount = (numBits - i);
+
+            uint32_t bitIndex = i + NUM_BITS_PER_BITFIELD_ELEMENT;
+            REQUIRE(bitfield.GetLongestRangeWithBitsSet(bitIndex) == expectedBitCount);
+        }
+
+        bitfield.Clear();
+
+        bitfield.SetRange(0, bitfieldSize - 1);
+
+        REQUIRE(bitfield.GetLongestRangeWithBitsSet(0) == bitfieldSize);
+    }
+
+    SECTION("Count number of continous bits set accross bitfield elements")
+    {
+        bitfield.SetBit(NUM_BITS_PER_BITFIELD_ELEMENT - 2);
+        bitfield.SetBit(NUM_BITS_PER_BITFIELD_ELEMENT - 1);
+        bitfield.SetBit(NUM_BITS_PER_BITFIELD_ELEMENT);
+        bitfield.SetBit(NUM_BITS_PER_BITFIELD_ELEMENT + 1);
+
+        uint32_t startingBitIndex = NUM_BITS_PER_BITFIELD_ELEMENT - 2;
+        for (uint32_t i = 0; i < 4; i++)
+        {
+            uint32_t expectedBitCount = (4 - i);
+
+            uint32_t bitIndex = startingBitIndex + i;
+            REQUIRE(bitfield.GetLongestRangeWithBitsSet(bitIndex) == expectedBitCount);
+        }
+    }
+}
---
+++ shipyard/system/bitfield.h
@@ -0,0 +1,266 @@
+#pragma once
+
+#include <system/common.h>
+
+namespace Shipyard
+{
+#if CPU_BITS == CPU_BITS_64
+#   define NUM_BITS_PER_BITFIELD_ELEMENT 64
+#elif CPU_BITS == CPU_BITS_32
+#   define NUM_BITS_PER_BITFIELD_ELEMENT 32
+#endif // #if CPU_BITS == CPU_BITS_64
+
+    template <uint32_t NumBits>
+    class Bitfield
+    {
+    private:
+
+#if CPU_BITS == CPU_BITS_64
+        using BitfieldType = uint64_t;
+#elif CPU_BITS == CPU_BITS_32
+        using BitfieldType = uint32_t;
+#endif // #if CPU_BITS == CPU_BITS_64
+
+        static constexpr uint32_t ms_NumElements = ((NumBits + NUM_BITS_PER_BITFIELD_ELEMENT - 1) / NUM_BITS_PER_BITFIELD_ELEMENT);
+
+    public:
+        Bitfield()
+        {
+            Clear();
+        }
+
+        void Clear()
+        {
+            for (uint32_t i = 0; i < ms_NumElements; i++)
+            {
+                m_BitField[i] = 0;
+            }
+        }
+
+        bool IsClear() const
+        {
+            for (uint32_t i = 0; i < ms_NumElements; i++)
+            {
+                if (m_BitField[i] != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        void SetBit(uint32_t bitIndex)
+        {
+            assert(bitIndex < NumBits);
+
+            uint32_t elementIndex = bitIndex / NUM_BITS_PER_BITFIELD_ELEMENT;
+            uint32_t bit = bitIndex % NUM_BITS_PER_BITFIELD_ELEMENT;
+
+            m_BitField[elementIndex] |= (BitfieldType(1) << BitfieldType(bit));
+        }
+
+        void UnsetBit(uint32_t bitIndex)
+        {
+            assert(bitIndex < NumBits);
+
+            uint32_t elementIndex = bitIndex / NUM_BITS_PER_BITFIELD_ELEMENT;
+            uint32_t bit = bitIndex % NUM_BITS_PER_BITFIELD_ELEMENT;
+
+            m_BitField[elementIndex] &= ~(BitfieldType(1) << BitfieldType(bit));
+        }
+
+        bool IsBitSet(uint32_t bitIndex) const
+        {
+            assert(bitIndex < NumBits);
+
+            uint32_t elementIndex = bitIndex / NUM_BITS_PER_BITFIELD_ELEMENT;
+            uint32_t bit = bitIndex % NUM_BITS_PER_BITFIELD_ELEMENT;
+
+            return ((m_BitField[elementIndex] & (BitfieldType(1) << BitfieldType(bit))) > 0);
+        }
+
+        void SetRange(uint32_t startingBitIndexInclusive, uint32_t endingBitIndexInclusive)
+        {
+            assert(startingBitIndexInclusive < NumBits);
+            assert(endingBitIndexInclusive < NumBits);
+
+            uint32_t startingElementIndex = startingBitIndexInclusive / NUM_BITS_PER_BITFIELD_ELEMENT;
+            uint32_t endingElementIndex = endingBitIndexInclusive / NUM_BITS_PER_BITFIELD_ELEMENT;
+
+            bool rangeInSingleElement = (startingBitIndexInclusive == endingElementIndex);
+            if (rangeInSingleElement)
+            {
+                BitfieldType maskOfBitsSetBeforeStart = ((BitfieldType(1) << BitfieldType(startingBitIndexInclusive)) - 1);
+
+                BitfieldType bitfieldAtEndingBit = (BitfieldType(1) << BitfieldType(endingBitIndexInclusive));
+                BitfieldType maskOfBitsSetBeforeEnd = ((bitfieldAtEndingBit - 1) | bitfieldAtEndingBit);
+
+                BitfieldType maskOfBitsToSet = (maskOfBitsSetBeforeStart ^ maskOfBitsSetBeforeEnd);
+
+                m_BitField[startingElementIndex] |= maskOfBitsToSet;
+            }
+            else
+            {
+                uint32_t elementIndex = startingElementIndex;
+
+                BitfieldType maskOfBitsToSet = ~((BitfieldType(1) << BitfieldType(startingBitIndexInclusive)) - 1);
+                m_BitField[elementIndex] |= maskOfBitsToSet;
+
+                elementIndex += 1;
+
+                for (; elementIndex <= endingElementIndex; elementIndex++)
+                {
+                    bool setFullRange = (elementIndex < endingElementIndex);
+
+                    if (setFullRange)
+                    {
+                        m_BitField[elementIndex] |= BitfieldType(-1);
+                    }
+                    else
+                    {
+                        BitfieldType bitfieldAtEndingBit = (BitfieldType(1) << BitfieldType(endingBitIndexInclusive));
+                        maskOfBitsToSet = ((bitfieldAtEndingBit - 1) | bitfieldAtEndingBit);
+
+                        m_BitField[elementIndex] |= maskOfBitsToSet;
+                    }
+                }
+            }
+        }
+
+        void UnsetRange(uint32_t startingBitIndexInclusive, uint32_t endingBitIndexInclusive)
+        {
+            assert(startingBitIndexInclusive < NumBits);
+            assert(endingBitIndexInclusive < NumBits);
+
+            uint32_t startingElementIndex = startingBitIndexInclusive / NUM_BITS_PER_BITFIELD_ELEMENT;
+            uint32_t endingElementIndex = endingBitIndexInclusive / NUM_BITS_PER_BITFIELD_ELEMENT;
+
+            bool rangeInSingleElement = (startingBitIndexInclusive == endingElementIndex);
+            if (rangeInSingleElement)
+            {
+                BitfieldType maskOfBitsSetBeforeStart = ((BitfieldType(1) << BitfieldType(startingBitIndexInclusive)) - 1);
+
+                BitfieldType bitfieldAtEndingBit = (BitfieldType(1) << BitfieldType(endingBitIndexInclusive));
+                BitfieldType maskOfBitsSetBeforeEnd = ((bitfieldAtEndingBit - 1) | bitfieldAtEndingBit);;
+
+                BitfieldType maskOfBitsToSet = (maskOfBitsSetBeforeStart ^ maskOfBitsSetBeforeEnd);
+
+                m_BitField[startingElementIndex] &= ~maskOfBitsToSet;
+            }
+            else
+            {
+                uint32_t elementIndex = startingElementIndex;
+
+                BitfieldType maskOfBitsToSet = ~((BitfieldType(1) << BitfieldType(startingBitIndexInclusive)) - 1);
+                m_BitField[elementIndex] &= ~maskOfBitsToSet;
+
+                elementIndex += 1;
+
+                for (; elementIndex <= endingElementIndex; elementIndex++)
+                {
+                    bool unsetFullRange = (elementIndex < endingElementIndex);
+                    if (unsetFullRange)
+                    {
+                        m_BitField[elementIndex] &= 0;
+                    }
+                    else
+                    {
+                        BitfieldType bitfieldAtEndingBit = (BitfieldType(1) << BitfieldType(endingBitIndexInclusive));
+                        maskOfBitsToSet = ((bitfieldAtEndingBit - 1) | bitfieldAtEndingBit);
+
+                        m_BitField[elementIndex] &= ~maskOfBitsToSet;
+                    }
+                }
+            }
+        }
+
+        // Finds the index of the first bit set, starting from the specified bit index.
+        // Returns false if a bit set to 1 after the starting bit index, including it, couldn't be found.
+        bool GetFirstBitSet(uint32_t startingBitIndex, uint32_t& firstBitSet) const
+        {
+            assert(startingBitIndex < NumBits);
+
+            uint32_t elementIndex = startingBitIndex / NUM_BITS_PER_BITFIELD_ELEMENT;
+            
+            BitfieldType maskOfEveryBitsBeforeCleared = ~((BitfieldType(1) << BitfieldType(startingBitIndex)) - 1);
+
+            unsigned long bitScanResult = (unsigned long)startingBitIndex;
+
+#if COMPILER == COMPILER_MSVC
+
+            for (; elementIndex < ms_NumElements; elementIndex++)
+            {
+                BitfieldType maskForScan = (m_BitField[elementIndex] & maskOfEveryBitsBeforeCleared);
+
+                unsigned char foundValue = FindFirstBitSet(&bitScanResult, maskForScan);
+
+                bool foundBitSet = (foundValue != 0);
+
+                if (foundBitSet)
+                {
+                    firstBitSet = uint32_t(bitScanResult) + elementIndex * NUM_BITS_PER_BITFIELD_ELEMENT;
+                    return true;
+                }
+                else
+                {
+                    maskOfEveryBitsBeforeCleared = BitfieldType(-1);
+                }
+            }
+#else
+
+#pragma error "Implement using intrinsic for bit scanning for specific compiler"
+
+#endif // #if COMPILER == COMPILER_MSVC
+
+            return false;
+        }
+
+        // Basically counts the number of bits set to 1 starting at the bit representing the given starting bit index.
+        // The starting bit is assumed to be set to 1 in the bitfield.
+        uint32_t GetLongestRangeWithBitsSet(uint32_t startingBitIndex) const
+        {
+            assert(startingBitIndex < NumBits);
+
+            uint32_t elementIndex = startingBitIndex / NUM_BITS_PER_BITFIELD_ELEMENT;
+            
+            BitfieldType maskOfEveryBitsBeforeSet = ((BitfieldType(1) << BitfieldType(startingBitIndex)) - 1);
+
+            uint32_t numBitsSet = 0;
+
+#if COMPILER == COMPILER_MSVC
+            for (; elementIndex < ms_NumElements; elementIndex++)
+            {
+                // Here, we reverse our bitfield, and scan for the first bit set to 1. This will be our first bit set to 0, which
+                // will tell us the end of our continous range of bits set to 1.
+                BitfieldType maskForScan = ~(m_BitField[elementIndex] | maskOfEveryBitsBeforeSet);
+
+                unsigned long firstBitSet = 0;
+                unsigned char foundValue = FindFirstBitSet(&firstBitSet, maskForScan);
+
+                bool foundBitSet = (foundValue != 0);
+
+                if (foundBitSet)
+                {
+                    numBitsSet = uint32_t(firstBitSet) + NUM_BITS_PER_BITFIELD_ELEMENT * elementIndex - startingBitIndex;
+                    break;
+                }
+                else
+                {
+                    maskOfEveryBitsBeforeSet = 0;
+                    numBitsSet += (NUM_BITS_PER_BITFIELD_ELEMENT - startingBitIndex);
+                }
+            }
+#else
+
+#pragma error "Implement using intrinsic for bit counting for specific compiler"
+
+#endif // #if COMPILER == COMPILER_MSVC
+
+            return numBitsSet;
+        }
+
+    private:
+        BitfieldType m_BitField[ms_NumElements];
+    };
+}

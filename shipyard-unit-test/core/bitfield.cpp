b4ee71464a9517a60319e05364c20aa6411eaa3f
Gauthier Viau <gauthierviau@gmail.com>
2019-01-02 07:47:19 -0500

    Added unit test for a bitfield with 16 bits i.e. less than 32/64, which would be a full bitfield's element

--- shipyard-unit-test/core/bitfield.cpp
+++ shipyard-unit-test/core/bitfield.cpp
@@ -371,3 +371,93 @@ TEST_CASE("Test bitfield 3 elements", "[Bitfield]")
         }
     }
 }
+
+TEST_CASE("Test bitfield less than an element", "[Bitfield]")
+{
+    constexpr uint32_t bitfieldSize = 16;
+    Shipyard::Bitfield<bitfieldSize> bitfield;
+
+    SECTION("Set bit")
+    {
+        REQUIRE(bitfield.IsClear());
+
+        bitfield.SetBit(0);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(0));
+
+        bitfield.UnsetBit(0);
+
+        REQUIRE(bitfield.IsClear());
+        REQUIRE(!bitfield.IsBitSet(0));
+
+        uint32_t lastBitIndex = (bitfieldSize - 1);
+        bitfield.SetBit(lastBitIndex);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(lastBitIndex));
+
+        bitfield.UnsetBit(lastBitIndex);
+
+        REQUIRE(bitfield.IsClear());
+        REQUIRE(!bitfield.IsBitSet(lastBitIndex));
+
+        bitfield.SetBit(10);
+
+        REQUIRE(!bitfield.IsClear());
+        REQUIRE(bitfield.IsBitSet(10));
+
+        bitfield.Clear();
+
+        REQUIRE(bitfield.IsClear());
+    }
+
+    SECTION("Get first bit set")
+    {
+        constexpr uint32_t numBits = 3;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            bitfield.SetBit(i);
+        }
+
+        uint32_t firstBitSet = 0;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            REQUIRE(bitfield.GetFirstBitSet(i, firstBitSet));
+            REQUIRE(firstBitSet == i);
+        }
+
+        REQUIRE(!bitfield.GetFirstBitSet(numBits, firstBitSet));
+
+        uint32_t bitIndex = 10;
+        bitfield.SetBit(bitIndex);
+
+        REQUIRE(bitfield.GetFirstBitSet(numBits, firstBitSet));
+        REQUIRE(firstBitSet == bitIndex);
+    }
+
+    SECTION("Count number of continous bits set")
+    {
+        constexpr uint32_t numBits = 3;
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            bitfield.SetBit(i);
+        }
+
+        for (uint32_t i = 0; i < numBits; i++)
+        {
+            uint32_t expectedBitCount = (numBits - i);
+
+            REQUIRE(bitfield.GetLongestRangeWithBitsSet(i) == expectedBitCount);
+        }
+
+        bitfield.Clear();
+
+        bitfield.SetRange(0, bitfieldSize - 1);
+
+        REQUIRE(bitfield.GetLongestRangeWithBitsSet(0) == bitfieldSize);
+    }
+}

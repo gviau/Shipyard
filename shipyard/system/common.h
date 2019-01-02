623f228cff74a7c053bc3efe372758cbf57d17a4
Gauthier Viau <gauthierviau@gmail.com>
2019-01-02 07:31:51 -0500

    Added some macros for windows' intrinsics for bit scanning

--- shipyard/system/common.h
+++ shipyard/system/common.h
@@ -1,7 +1,23 @@
 #pragma once
 
+#include <system/platform.h>
+
+
+#if PLATFORM == PLATFORM_WINDOWS
+#include <intrin.h>
+#endif // #if PLATFORM == PLATFORM_WINDOWS
+
 namespace Shipyard
 {
 #define MIN(a, b) ((a < b) ? a : b)
 #define MAX(a, b) ((a > b) ? a : b)
+
+    // Sone intrinsics as macros for convenience
+#if COMPILER == COMPILER_MSVC
+#   if CPU_BITS == CPU_BITS_64
+#       define FindFirstBitSet(pOutFirstBitSet, scanMask) _BitScanForward64(pOutFirstBitSet, scanMask)
+#   elif CPU_BITS == CPU_BITS_32
+#       define FindFirstBitSet(pOutFirstBitSet, scanMask) _BitScanForward(pOutFirstBitSet, scanMask)
+#   endif // #if CPU_BITS == CPU_BITS_64
+#endif // #if COMPILER == COMPILER_MSVC
 }

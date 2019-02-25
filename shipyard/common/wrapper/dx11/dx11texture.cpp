9c0144d77a7d467a2d79d575a2de4b7e266e8ed7
Gauthier Viau <gauthierviau@gmail.com>
2019-02-25 07:15:55 -0500

    * The texture class now properly creates render target and depth stencil render target * It also properly handles the case where a depth stencil render target is created as a shader resource view, since the depth formats are not supposed to be used as shader resource views. We instead have to work around it by creating a typeless texture, creating a normal shader resource view and depth format depth stencil view * Added a constructor to pass in an already constructed ID3D11Texture2D, so that the swap chain can still use the RenderTarget class, which uses GFXTexture2D

--- shipyard/common/wrapper/dx11/dx11texture.cpp
+++ shipyard/common/wrapper/dx11/dx11texture.cpp
@@ -14,8 +14,16 @@
 namespace Shipyard
 {;
 
-DX11BaseTexture::DX11BaseTexture(ID3D11Device& device, uint32_t width, uint32_t height, GfxFormat pixelFormat, bool dynamic, void* initialData, bool generateMips)
-    : BaseTexture(width, height, pixelFormat)
+DX11BaseTexture::DX11BaseTexture(
+        ID3D11Device& device,
+        uint32_t width,
+        uint32_t height,
+        GfxFormat pixelFormat,
+        bool dynamic,
+        void* initialData,
+        bool generateMips,
+        TextureUsage textureUsage)
+    : BaseTexture(width, height, pixelFormat, textureUsage)
     , m_ShaderResourceView(nullptr)
 {
 
@@ -29,10 +37,41 @@ DX11BaseTexture::~DX11BaseTexture()
     }
 }
 
-ID3D11ShaderResourceView* DX11BaseTexture::CreateShaderResourceView(ID3D11Device& device, ID3D11Resource& resource, GfxFormat pixelFormat, TextureViewType textureViewType)
+ID3D11ShaderResourceView* DX11BaseTexture::CreateShaderResourceView(ID3D11Device& device, ID3D11Resource& resource, GfxFormat pixelFormat, TextureViewType textureViewType, TextureUsage textureUsage)
 {
     D3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc;
-    shaderResourceViewDesc.Format = ConvertShipyardFormatToDX11(pixelFormat);
+
+    // A depth stencil view with a depth format cannot be created as a shader resource view. Instead we, need to use
+    // an equivalent non-depth format and create the depth stencil view with the proper depth format.
+    if ((textureUsage & TextureUsage::TextureUsage_DepthStencil) > 0)
+    {
+        switch (pixelFormat)
+        {
+        case GfxFormat::D16_UNORM:
+            shaderResourceViewDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R16_UNORM;
+            break;
+
+        case GfxFormat::D24_UNORM_S8_UINT:
+            shaderResourceViewDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R24_UNORM_X8_TYPELESS;
+            break;
+
+        case GfxFormat::D32_FLOAT:
+            shaderResourceViewDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R32_FLOAT;
+            break;
+
+        case GfxFormat::D32_FLOAT_S8X24_UINT:
+            shaderResourceViewDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R32_FLOAT_X8X24_TYPELESS;
+            break;
+
+        default:
+            shaderResourceViewDesc.Format = ConvertShipyardFormatToDX11(pixelFormat);
+            break;
+        }
+    }
+    else
+    {
+        shaderResourceViewDesc.Format = ConvertShipyardFormatToDX11(pixelFormat);
+    }
 
     switch (textureViewType)
     {
@@ -57,8 +96,16 @@ ID3D11ShaderResourceView* DX11BaseTexture::CreateShaderResourceView(ID3D11Device
     return shaderResourceView;
 }
 
-DX11Texture2D::DX11Texture2D(ID3D11Device& device, uint32_t width, uint32_t height, GfxFormat pixelFormat, bool dynamic, void* initialData, bool generateMips)
-    : DX11BaseTexture(device, width, height, pixelFormat, dynamic, initialData, generateMips)
+DX11Texture2D::DX11Texture2D(
+        ID3D11Device& device,
+        uint32_t width,
+        uint32_t height,
+        GfxFormat pixelFormat,
+        bool dynamic,
+        void* initialData,
+        bool generateMips,
+        TextureUsage textureUsage)
+    : DX11BaseTexture(device, width, height, pixelFormat, dynamic, initialData, generateMips, textureUsage)
     , m_Texture(nullptr)
 {
     D3D11_USAGE usage = (dynamic ? D3D11_USAGE_DYNAMIC : D3D11_USAGE_DEFAULT);
@@ -76,21 +123,79 @@ DX11Texture2D::DX11Texture2D(ID3D11Device& device, uint32_t width, uint32_t heig
     textureDesc.MiscFlags = 0;
     textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
 
-    uint32_t rowPitch = GetRowPitch(width, pixelFormat);
+    if ((textureUsage & TextureUsage::TextureUsage_RenderTarget) > 0)
+    {
+        textureDesc.BindFlags |= D3D11_BIND_RENDER_TARGET;
+    }
 
-    D3D11_SUBRESOURCE_DATA data;
-    data.pSysMem = initialData;
-    data.SysMemPitch = rowPitch;
-    data.SysMemSlicePitch = rowPitch * height;
+    if ((textureUsage & TextureUsage::TextureUsage_DepthStencil) > 0)
+    {
+        textureDesc.BindFlags |= D3D11_BIND_DEPTH_STENCIL;
 
-    HRESULT hr = device.CreateTexture2D(&textureDesc, &data, &m_Texture);
+        // A depth stencil view with a depth format cannot be created as a shader resource view. Instead we, need to use
+        // an equivalent non-depth format and create the depth stencil view with the proper depth format.
+        switch (pixelFormat)
+        {
+        case GfxFormat::D16_UNORM:
+            textureDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R16_TYPELESS;
+            break;
+
+        case GfxFormat::D24_UNORM_S8_UINT:
+            textureDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R24G8_TYPELESS;
+            break;
+
+        case GfxFormat::D32_FLOAT:
+            textureDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R32_TYPELESS;
+            break;
+
+        case GfxFormat::D32_FLOAT_S8X24_UINT:
+            textureDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R32G8X24_TYPELESS;
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    HRESULT hr;
+    if (initialData != nullptr)
+    {
+        uint32_t rowPitch = GetRowPitch(width, pixelFormat);
+
+        D3D11_SUBRESOURCE_DATA data;
+        data.pSysMem = initialData;
+        data.SysMemPitch = rowPitch;
+        data.SysMemSlicePitch = rowPitch * height;
+
+        hr = device.CreateTexture2D(&textureDesc, &data, &m_Texture);
+    }
+    else
+    {
+        hr = device.CreateTexture2D(&textureDesc, nullptr, &m_Texture);
+    }
+
     if (FAILED(hr))
     {
         MessageBox(NULL, "CreateTexture2D failed", "DX11 error", MB_OK);
         return;
     }
 
-    m_ShaderResourceView = DX11BaseTexture::CreateShaderResourceView(device, *m_Texture, m_PixelFormat, TextureViewType::Tex2D);
+    m_ShaderResourceView = DX11BaseTexture::CreateShaderResourceView(device, *m_Texture, m_PixelFormat, TextureViewType::Tex2D, textureUsage);
+}
+
+DX11Texture2D::DX11Texture2D(ID3D11Device& device, ID3D11Texture2D& texture, GfxFormat format)
+    : DX11BaseTexture(device, 0, 0, format, false, nullptr, false, TextureUsage::TextureUsage_RenderTarget)
+    , m_Texture(nullptr)
+{
+    m_Texture = &texture;
+
+    m_Texture->AddRef();
+
+    D3D11_TEXTURE2D_DESC textureDesc;
+    m_Texture->GetDesc(&textureDesc);
+
+    m_Width = textureDesc.Width;
+    m_Height = textureDesc.Height;
 }
 
 DX11Texture2D::~DX11Texture2D()
--- shipyard/common/wrapper/dx11/dx11texture.h
+++ shipyard/common/wrapper/dx11/dx11texture.h
@@ -20,12 +20,20 @@ namespace Shipyard
     class SHIPYARD_API DX11BaseTexture : public BaseTexture
     {
     public:
-        DX11BaseTexture(ID3D11Device& device, uint32_t width, uint32_t height, GfxFormat pixelFormat, bool dynamic, void* initialData, bool generateMips);
+        DX11BaseTexture(
+                ID3D11Device& device,
+                uint32_t width,
+                uint32_t height,
+                GfxFormat pixelFormat,
+                bool dynamic,
+                void* initialData,
+                bool generateMips,
+                TextureUsage textureUsage = TextureUsage::TextureUsage_Default);
         ~DX11BaseTexture();
 
         ID3D11ShaderResourceView* GetShaderResourceView() const { return m_ShaderResourceView; }
 
-        static ID3D11ShaderResourceView* CreateShaderResourceView(ID3D11Device& device, ID3D11Resource& resource, GfxFormat pixelFormat, TextureViewType textureViewType);
+        static ID3D11ShaderResourceView* CreateShaderResourceView(ID3D11Device& device, ID3D11Resource& resource, GfxFormat pixelFormat, TextureViewType textureViewType, TextureUsage textureUsage = TextureUsage::TextureUsage_Default);
 
     protected:
         ID3D11ShaderResourceView* m_ShaderResourceView;
@@ -34,7 +42,18 @@ namespace Shipyard
     class SHIPYARD_API DX11Texture2D : public Texture2D, public DX11BaseTexture
     {
     public:
-        DX11Texture2D(ID3D11Device& device, uint32_t width, uint32_t height, GfxFormat pixelFormat, bool dynamic, void* initialData, bool generateMips);
+        DX11Texture2D(
+                ID3D11Device& device,
+                uint32_t width,
+                uint32_t height,
+                GfxFormat pixelFormat,
+                bool dynamic,
+                void* initialData,
+                bool generateMips,
+                TextureUsage textureUsage = TextureUsage::TextureUsage_Default);
+
+        DX11Texture2D(ID3D11Device& device, ID3D11Texture2D& texture, GfxFormat format);
+
         ~DX11Texture2D();
 
         ID3D11Texture2D* GetTexture() const { return m_Texture; }

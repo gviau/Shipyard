cfd4d7af165ae377a1da1399be7127eff0f5f9c3
Gauthier Viau <gauthierviau@gmail.com>
2019-03-26 07:22:56 -0400

    Added a pause at the end of the generate_projects.bat script with there's no argument to the script

--- generate_projects.bat
+++ generate_projects.bat
@@ -5,3 +5,11 @@ cd /D "%~dp0"
 sharpmake-scripts\sharpmake-binaries\Sharpmake.Application.exe /sources("sharpmake-scripts/Program.cs")
 
 popd
+
+if "%1" NEQ "" (
+	goto nopause
+)
+
+pause
+	
+:nopause

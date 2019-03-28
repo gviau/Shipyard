d7cf3ab02f5a12e27822a3763dae878579f2cd73
Gauthier Viau <gauthierviau@gmail.com>
2019-03-28 10:39:10 -0400

    * Added variadic arguments to logging class. * Disable logging in master builds

--- shipyard/system/logger.cpp
+++ shipyard/system/logger.cpp
@@ -1,5 +1,7 @@
 #include <system/logger.h>
 
+#ifdef SHIP_ENABLE_LOGGING
+
 #include <system/systemcommon.h>
 
 namespace Shipyard
@@ -48,7 +50,20 @@ void GetCurrentTimeForLog(StringA& currentTimeForLog)
     currentTimeForLog += " - ";
 }
 
-void Logger::LogDebug(const StringA& message)
+void Log(std::ofstream& file, const char* pHeader, const char* pMessage, va_list argPtr)
+{
+    StringA currentTime;
+    GetCurrentTimeForLog(currentTime);
+
+    constexpr int maxCount = 4096;
+    char pBuffer[maxCount];
+
+    vsnprintf_s(pBuffer, maxCount, maxCount - 1, pMessage, argPtr);
+
+    file << currentTime.GetBuffer() << pHeader << pBuffer << std::endl;
+}
+
+void Logger::LogDebug(const char* pMessage, ...)
 {
     if ((m_LogLevel & LogLevel::LogLevel_Debug) == 0)
     {
@@ -57,15 +72,17 @@ void Logger::LogDebug(const StringA& message)
 
     m_LoggerLock.lock();
 
-    StringA currentTime;
-    GetCurrentTimeForLog(currentTime);
+    va_list argsPtr;
+    va_start(argsPtr, pMessage);
 
-    m_LogFile << currentTime.GetBuffer() << " [DEBUG]   " << message.GetBuffer() << std::endl;
+    Log(m_LogFile, " [DEBUG]   ", pMessage, argsPtr);
+
+    va_end(argsPtr);
 
     m_LoggerLock.unlock();
 }
 
-void Logger::LogInfo(const StringA& message)
+void Logger::LogInfo(const char* pMessage, ...)
 {
     if ((m_LogLevel & LogLevel::LogLevel_Info) == 0)
     {
@@ -74,15 +91,17 @@ void Logger::LogInfo(const StringA& message)
 
     m_LoggerLock.lock();
 
-    StringA currentTime;
-    GetCurrentTimeForLog(currentTime);
+    va_list argsPtr;
+    va_start(argsPtr, pMessage);
 
-    m_LogFile << currentTime.GetBuffer() << " [INFO]    " << message.GetBuffer() << std::endl;
+    Log(m_LogFile, " [INFO]    ", pMessage, argsPtr);
+
+    va_end(argsPtr);
 
     m_LoggerLock.unlock();
 }
 
-void Logger::LogWarning(const StringA& message)
+void Logger::LogWarning(const char* pMessage, ...)
 {
     if ((m_LogLevel & LogLevel::LogLevel_Warning) == 0)
     {
@@ -91,15 +110,17 @@ void Logger::LogWarning(const StringA& message)
 
     m_LoggerLock.lock();
 
-    StringA currentTime;
-    GetCurrentTimeForLog(currentTime);
+    va_list argsPtr;
+    va_start(argsPtr, pMessage);
 
-    m_LogFile << currentTime.GetBuffer() << " [WARNING] " << message.GetBuffer() << std::endl;
+    Log(m_LogFile, " [WARNING] ", pMessage, argsPtr);
+
+    va_end(argsPtr);
 
     m_LoggerLock.unlock();
 }
 
-void Logger::LogError(const StringA& message)
+void Logger::LogError(const char* pMessage, ...)
 {
     if ((m_LogLevel & LogLevel::LogLevel_Error) == 0)
     {
@@ -108,12 +129,16 @@ void Logger::LogError(const StringA& message)
 
     m_LoggerLock.lock();
 
-    StringA currentTime;
-    GetCurrentTimeForLog(currentTime);
+    va_list argsPtr;
+    va_start(argsPtr, pMessage);
 
-    m_LogFile << currentTime.GetBuffer() << " [ERROR]   " << message.GetBuffer() << std::endl;
+    Log(m_LogFile, " [ERROR]   ", pMessage, argsPtr);
+
+    va_end(argsPtr);
 
     m_LoggerLock.unlock();
 }
 
 }
+
+#endif // #ifdef SHIP_ENABLE_LOGGING
--- shipyard/system/logger.h
+++ shipyard/system/logger.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifndef SHIP_MASTER
+#define SHIP_ENABLE_LOGGING
+#endif // #ifndef SHIP_MASTER
+
+#ifdef SHIP_ENABLE_LOGGING
+
 #include <system/singleton.h>
 
 #include <system/string.h>
@@ -7,6 +13,8 @@
 #include <fstream>
 
 #include <mutex>
+
+#include <stdarg.h>
 
 namespace Shipyard
 {
@@ -33,10 +41,10 @@ namespace Shipyard
 
         void SetLogLevel(LogLevel logLevel);
 
-        void LogDebug(const StringA& message);
-        void LogInfo(const StringA& message);
-        void LogWarning(const StringA& message);
-        void LogError(const StringA& message);
+        void LogDebug(const char* pMessage, ...);
+        void LogInfo(const char* pMessage, ...);
+        void LogWarning(const char* pMessage, ...);
+        void LogError(const char* pMessage, ...);
 
     private:
         std::ofstream m_LogFile;
@@ -46,7 +54,16 @@ namespace Shipyard
     };
 }
 
-#define SHIP_LOG_DEBUG(msg) Shipyard::Logger::GetInstance().LogDebug(msg)
-#define SHIP_LOG_INFO(msg) Shipyard::Logger::GetInstance().LogInfo(msg)
-#define SHIP_LOG_WARNING(msg) Shipyard::Logger::GetInstance().LogWarning(msg)
-#define SHIP_LOG_ERROR(msg) Shipyard::Logger::GetInstance().LogError(msg)
+#define SHIP_LOG_DEBUG(msg, ...) Shipyard::Logger::GetInstance().LogDebug(msg, __VA_ARGS__)
+#define SHIP_LOG_INFO(msg, ...) Shipyard::Logger::GetInstance().LogInfo(msg, __VA_ARGS__)
+#define SHIP_LOG_WARNING(msg, ...) Shipyard::Logger::GetInstance().LogWarning(msg, __VA_ARGS__)
+#define SHIP_LOG_ERROR(msg, ...) Shipyard::Logger::GetInstance().LogError(msg, __VA_ARGS__)
+
+#else
+
+#define SHIP_LOG_DEBUG(msg, ...)
+#define SHIP_LOG_INFO(msg, ...)
+#define SHIP_LOG_WARNING(msg, ...)
+#define SHIP_LOG_ERROR(msg, ...)
+
+#endif // #ifdef SHIP_ENABLE_LOGGING

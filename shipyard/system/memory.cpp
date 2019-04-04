70c07948c37e6a78cc225f3e1a866b444d950697
gviau <gauthier.viau@siliconstudio.co.jp>
2019-04-04 14:54:39 +0900

    * Added the GlobalAllocator singleton. * It's role is to aggregate several allocators and determine which one to use based on the requested allocation size. * It expects already created allocators, in increasing order of maximum allocation sizes. * The class doesn't use the Singleton<T> class, because it needs to be allocated on the stack, wheras right now, the Singleton base class allocates on the heap. * Added macros for allocating memory that do not require an allocator, which will use the GlobalAlloactor instead.

---
+++ shipyard/system/memory.cpp
@@ -0,0 +1,128 @@
+#include <system/memory.h>
+
+#include <system/systemdebug.h>
+
+namespace Shipyard
+{;
+
+GlobalAllocator::GlobalAllocator()
+    : m_NumAllocators(0)
+
+#ifdef SHIP_DEBUG
+    , m_Initialized(false)
+#endif // #ifdef SHIP_DEBUG
+{
+
+}
+
+GlobalAllocator::~GlobalAllocator()
+{
+
+}
+
+bool GlobalAllocator::Create(AllocatorInitEntry* pInitEntries, uint32_t numAllocators)
+{
+    SHIP_ASSERT(pInitEntries != nullptr);
+    SHIP_ASSERT(numAllocators > 0);
+
+    size_t lastSize = 0;
+
+    for (uint32_t i = 0; i < numAllocators ; i++)
+    {
+        const AllocatorInitEntry& allocatorInitEntry = pInitEntries[i];
+
+        SHIP_ASSERT(allocatorInitEntry.pAllocator != nullptr);
+
+        SHIP_ASSERT_MSG(lastSize < allocatorInitEntry.maxAllocationSize, "GlobalAllocator::Create --> Allocators are assumed to be in increasing order");
+
+        lastSize = allocatorInitEntry.maxAllocationSize;
+
+        if (i == (numAllocators - 1))
+        {
+            m_MaxAllocationSizes[i] = size_t(-1);
+        }
+        else
+        {
+            m_MaxAllocationSizes[i] = allocatorInitEntry.maxAllocationSize;
+        }
+
+        AllocatorAddressRange& allocatorAddressRange = m_pAllocators[i];
+        allocatorAddressRange.pAllocator = allocatorInitEntry.pAllocator;
+        allocatorAddressRange.startingAddressBytes = size_t(allocatorInitEntry.pAllocator->GetHeap());
+        allocatorAddressRange.endingAddressBytes = allocatorAddressRange.startingAddressBytes + allocatorInitEntry.pAllocator->GetHeapSize();
+
+        SHIP_ASSERT(allocatorAddressRange.startingAddressBytes < allocatorAddressRange.endingAddressBytes);
+    }
+
+    m_NumAllocators = numAllocators;
+
+#ifdef SHIP_DEBUG
+    m_Initialized = true;
+#endif // #ifdef SHIP_DEBUG
+
+    return true;
+}
+
+void GlobalAllocator::Destroy()
+{
+    m_NumAllocators = 0;
+
+#ifdef SHIP_DEBUG
+    m_Initialized = false;
+#endif // #ifdef SHIP_DEBUG
+}
+
+void* GlobalAllocator::Allocate(size_t size, size_t alignment
+
+        #ifdef SHIP_ALLOCATOR_DEBUG_INFO
+            , const char* pAllocationFilename
+            , int allocationLineNumber
+        #endif // #ifdef SHIP_ALLOCATOR_DEBUG_INFO
+
+        )
+{
+#ifdef SHIP_DEBUG
+    SHIP_ASSERT_MSG(m_Initialized, "The GlobalAllocator needs to be initialized before using it for allocations!");
+#endif // #ifdef SHIP_DEBUG
+
+    uint32_t allocatorIndexToUse = 0;
+
+    for (; allocatorIndexToUse < m_NumAllocators; allocatorIndexToUse++)
+    {
+        if (size <= m_MaxAllocationSizes[allocatorIndexToUse])
+        {
+            break;
+        }
+    }
+
+    return m_pAllocators[allocatorIndexToUse].pAllocator->Allocate(size, alignment
+
+        #ifdef SHIP_ALLOCATOR_DEBUG_INFO
+            , pAllocationFilename
+            , allocationLineNumber
+        #endif // #ifdef SHIP_ALLOCATOR_DEBUG_INFO
+
+        );
+}
+
+void GlobalAllocator::Deallocate(void* memory)
+{
+#ifdef SHIP_DEBUG
+    SHIP_ASSERT_MSG(m_Initialized, "The GlobalAllocator needs to be initialized before using it for freeing memory!");
+#endif // #ifdef SHIP_DEBUG
+
+    size_t memoryAddress = size_t(memory);
+
+    for (uint32_t i = 0; i < m_NumAllocators; i++)
+    {
+        AllocatorAddressRange& allocatorAddressRange = m_pAllocators[i];
+
+        if (memoryAddress >= allocatorAddressRange.startingAddressBytes && memoryAddress <= allocatorAddressRange.endingAddressBytes)
+        {
+            allocatorAddressRange.pAllocator->Deallocate(memory);
+            break;
+        }
+    }
+}
+
+}
--- shipyard/system/memory.h
+++ shipyard/system/memory.h
@@ -1,9 +1,98 @@
 #pragma once
 
+#include <system/memory/baseallocator.h>
+
 namespace Shipyard
 {
-#define MemAlloc(objType) new objType
-#define MemArrayAlloc(objType, numElements) new objType[numElements]
-#define MemFree(ptr) delete ptr
-#define MemArrayFree(ptr) delete[] ptr
+    class SHIPYARD_API GlobalAllocator : public BaseAllocator
+    {
+    public:
+        static const uint32_t ms_MaxNumAllocators = 32;
+
+        struct AllocatorInitEntry
+        {
+            BaseAllocator* pAllocator = nullptr;
+
+            // maxAllocationSize is the size in bytes for up to which the allocator will be considered.
+            size_t maxAllocationSize = 0;
+        };
+
+    public:
+
+        static GlobalAllocator& GetInstance()
+        {
+            static GlobalAllocator s_GlobalAllocator;
+            return s_GlobalAllocator;
+        }
+
+        // Allocators are all assumed to be created beforehand.
+        // During allocations, first matching allocator found will be used. Therefore, allocators are assumed to
+        // be in increasing maximum size. Last allocator's maximum size is ignored since its assumed to be the fallback allocator.
+        bool Create(AllocatorInitEntry* pInitEntries, uint32_t numAllocators);
+
+        void Destroy();
+
+        // Alignment must be a power of 2 and non-zero.
+        virtual void* Allocate(size_t size, size_t alignment
+
+                #ifdef SHIP_ALLOCATOR_DEBUG_INFO
+                    , const char* pAllocationFilename
+                    , int allocationLineNumber
+                #endif // #ifdef SHIP_ALLOCATOR_DEBUG_INFO
+
+            ) override;
+
+        // Memory must come from the allocator that allocated it.
+        virtual void Deallocate(void* memory) override;
+
+    private:
+        struct AllocatorAddressRange
+        {
+            BaseAllocator* pAllocator = nullptr;
+            size_t startingAddressBytes = 0;
+            size_t endingAddressBytes = 0;
+        };
+
+    private:
+        GlobalAllocator();
+        ~GlobalAllocator();
+
+        GlobalAllocator(const GlobalAllocator& src) = delete;
+        GlobalAllocator& operator= (const GlobalAllocator& rhs) = delete;
+        GlobalAllocator& operator= (const GlobalAllocator&& rhs) = delete;
+
+        size_t m_MaxAllocationSizes[ms_MaxNumAllocators];
+        AllocatorAddressRange m_pAllocators[ms_MaxNumAllocators];
+        uint32_t m_NumAllocators;
+
+#ifdef SHIP_DEBUG
+        // Used to assert when we forget to initialize this guy before usage.
+        bool m_Initialized;
+#endif // #ifdef SHIP_DEBUG
+    };
+
+    // Those macros use the GlobalAllocator instead of specifying one. Be sure not to mismatch allocators!
+#ifdef SHIP_ALLOCATOR_DEBUG_INFO
+
+#define SHIP_ALLOC(size, alignment) GlobalAllocator::GetInstance().Allocate(size, alignment, __FILE__, __LINE__)
+
+#define SHIP_NEW(classX, alignment) new(GlobalAllocator::GetInstance().Allocate(sizeof(classX), alignment, __FILE__, __LINE__))classX
+
+#define SHIP_NEW_ARRAY(classX, length, alignment) NewArray<classX>(&GlobalAllocator::GetInstance(), length, alignment, __FILE__, __LINE__)
+
+#else
+
+#define SHIP_ALLOC(size, alignment) GlobalAllocator::GetInstance().Allocate(size, alignment)
+
+#define SHIP_NEW(classX, alignment) new(GlobalAllocator::GetInstance().Allocate(sizeof(classX), alignment))classX
+
+#define SHIP_NEW_ARRAY(classX, length, alignment) NewArray<classX>(&GlobalAllocator::GetInstance(), length, alignment)
+
+#endif // #ifdef SHIP_ALLOCATOR_DEBUG_INFO
+
+#define SHIP_FREE(memory) if (memory != nullptr) GlobalAllocator::GetInstance().Deallocate(memory)
+
+#define SHIP_DELETE(memory) if (memory != nullptr) { DestructorCall(memory); GlobalAllocator::GetInstance().Deallocate(memory); }
+
+#define SHIP_DELETE_ARRAY(memory) if (memory != nullptr) DeleteArray(&GlobalAllocator::GetInstance(), memory)
 }

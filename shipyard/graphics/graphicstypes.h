b28d2e0f66964c23d7793db0c630ff5b1e3775df
Gauthier Viau <gauthierviau@gmail.com>
2019-08-04 12:05:57 -0400

    * Use a macro to define the various graphics types. * Added a comparison operator to each graphics type handle

--- shipyard/graphics/graphicstypes.h
+++ shipyard/graphics/graphicstypes.h
@@ -6,224 +6,33 @@ namespace Shipyard
 {
     const shipUint32 InvalidGfxHandle = 0xffffffff;
 
-    struct GFXVertexBufferHandle
-    {
-        GFXVertexBufferHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXVertexBufferHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXIndexBufferHandle
-    {
-        GFXIndexBufferHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXIndexBufferHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXConstantBufferHandle
-    {
-        GFXConstantBufferHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXConstantBufferHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXByteBufferHandle
-    {
-        GFXByteBufferHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXByteBufferHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXDescriptorSetHandle
-    {
-        GFXDescriptorSetHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXDescriptorSetHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXPipelineStateObjectHandle
-    {
-        GFXPipelineStateObjectHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXPipelineStateObjectHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXRenderTargetHandle
-    {
-        GFXRenderTargetHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXRenderTargetHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXDepthStencilRenderTargetHandle
-    {
-        GFXDepthStencilRenderTargetHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXDepthStencilRenderTargetHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXRootSignatureHandle
-    {
-        GFXRootSignatureHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXRootSignatureHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXVertexShaderHandle
-    {
-        GFXVertexShaderHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXVertexShaderHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXPixelShaderHandle
-    {
-        GFXPixelShaderHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXPixelShaderHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXTexture2DHandle
-    {
-        GFXTexture2DHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXTexture2DHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
-
-    struct GFXSamplerHandle
-    {
-        GFXSamplerHandle()
-            : handle(InvalidGfxHandle)
-        {
-
-        }
-
-        GFXSamplerHandle(shipUint32 _handle)
-            : handle(_handle)
-        {
-
-        }
-
-        shipUint32 handle;
-    };
+#define DEFINE_HANDLE_TYPE(HandleTypeName) \
+    struct HandleTypeName \
+    { \
+        HandleTypeName() \
+            : handle(InvalidGfxHandle) \
+        { } \
+        HandleTypeName(shipUint32 _handle) \
+            : handle(_handle) \
+        { } \
+        shipBool operator== (const HandleTypeName& rhs) const \
+        { \
+            return handle == rhs.handle; \
+        } \
+        shipUint32 handle; \
+    }
+
+    DEFINE_HANDLE_TYPE(GFXVertexBufferHandle);
+    DEFINE_HANDLE_TYPE(GFXIndexBufferHandle);
+    DEFINE_HANDLE_TYPE(GFXConstantBufferHandle);
+    DEFINE_HANDLE_TYPE(GFXByteBufferHandle);
+    DEFINE_HANDLE_TYPE(GFXDescriptorSetHandle);
+    DEFINE_HANDLE_TYPE(GFXPipelineStateObjectHandle);
+    DEFINE_HANDLE_TYPE(GFXRenderTargetHandle);
+    DEFINE_HANDLE_TYPE(GFXDepthStencilRenderTargetHandle);
+    DEFINE_HANDLE_TYPE(GFXRootSignatureHandle);
+    DEFINE_HANDLE_TYPE(GFXVertexShaderHandle);
+    DEFINE_HANDLE_TYPE(GFXPixelShaderHandle);
+    DEFINE_HANDLE_TYPE(GFXTexture2DHandle);
+    DEFINE_HANDLE_TYPE(GFXSamplerHandle);
 }

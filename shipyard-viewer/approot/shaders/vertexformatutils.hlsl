8304ee58c002e9f1e157a4277dab4ecba490f1ca
gviau <gauthierviau@gmail.com>
2019-12-11 20:07:03 +0100

    * Small indentation changes

--- shipyard-viewer/approot/shaders/vertexformatutils.hlsl
+++ shipyard-viewer/approot/shaders/vertexformatutils.hlsl
@@ -17,16 +17,16 @@ struct VertexData
 
 void UnpackVertexInput(in vs_input vertexInput, out VertexData vertexData)
 {
-	vertexData = (VertexData)0;
-	
+    vertexData = (VertexData)0;
+    
 #if VERTEX_FORMAT_TYPE == 5
-	vertexData.position = float3(vertexInput.position.xy, 0.0);
+    vertexData.position = float3(vertexInput.position.xy, 0.0);
 #else
     vertexData.position = vertexInput.position.xyz;
 #endif // #if VERTEX_FORMAT_TYPE == 5
 
 #ifdef VERTEX_FORMAT_HAS_TEXCOORDS
-    vertexData.texCoords = vertexInput.tex_coords.xy;
+    vertexData.texCoords.xy = vertexInput.tex_coords.xy;
 #endif // #ifdef VERTEX_FORMAT_HAS_TEXCOORDS
 
 #ifdef VERTEX_FORMAT_HAS_COLOR
